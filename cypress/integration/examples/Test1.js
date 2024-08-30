08867dfe8165c6714113fcb3b55e88193540517a
Naveen Raju <naveen.raju@ATMECS.CORP>
2024-08-30 14:57:11 +0530

    First commit

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+describe('My First Test Suite', function()
+{
+    it('My First Test Case', function()
+{
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    cy.get('.product:visible').should('have.length',4)
+    
+
+
+
+})
+})
