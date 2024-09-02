e6b4ea55d9a1d982536eba52c4940ba67a003d1f
Naveen Raju <naveen.raju@ATMECS.CORP>
2024-09-02 18:24:42 +0530

    september2

---
+++ cypress/integration/examples/Test2.js
@@ -0,0 +1,30 @@
+describe('My Second Test Suite', function() 
+{
+ 
+it('My SecondTest case',function() {
+ 
+ 
+cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+cy.get('.search-keyword').type('ca')
+cy.wait(2000)
+ 
+//Parent child chaining
+cy.get('.products').as('productLocator')
+cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+ 
+const textVeg=$el.find('h4.product-name').text()
+if(textVeg.includes('Cashews'))
+{
+$el.find('button').click()
+}
+})
+cy.get('.cart-icon > img').click()
+ 
+ 
+ 
+ 
+}  )
+ 
+
+ 
+}  )
